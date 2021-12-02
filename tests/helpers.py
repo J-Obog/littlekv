571de78d871b8f4ff5aec2e5ef2adb0e33a903d7
JObog <jobogbaimhe@gmail.com>
2021-12-01 23:10:34 -0500

    helpers now delimits command strings by space

--- tests/helpers.py
+++ tests/helpers.py
@@ -7,13 +7,13 @@ import sys
 
 def launch_client_proc(cmd: str, conn_delay: int = 1) -> Tuple[int, int, str]:
     time.sleep(conn_delay)
-    client_proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    client_proc = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     exit_code = client_proc.wait()
     output = client_proc.communicate()[0].decode('utf')
     return (client_proc.pid, exit_code, output)
 
 def launch_server_proc(cmd: str) -> Tuple[int]:
-    server_proc = subprocess.Popen('lkv-server')
+    server_proc = subprocess.Popen(cmd.split())
     return server_proc.pid
 
 def kill_server_proc(pid: str, timeout: int = 1):
