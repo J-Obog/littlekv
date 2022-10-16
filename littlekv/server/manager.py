dcbd31df5c7e02b26edb3892cae72d3378e74bc0
JObog <jobogbaimhe@gmail.com>
2022-10-16 15:45:51 -0400

    implementing manager methods

--- littlekv/server/manager.py
+++ littlekv/server/manager.py
@@ -1,13 +1,22 @@
 from littlekv.store import Store
-
+from typing import Dict
 
 class LittleKVManager:
     def __init__(self, store: Store):
         self._store = store
         self._hash_map = store.read()
 
-    def get(self):
-        pass
+    def get(self, key: str) -> str:
+        return self._hash_map.get(key, None)
 
-    def set(self):
-        pass
+    def get_all(self) -> Dict[str,str]:
+        return dict(self._hash_map)
+
+    def set(self, key: str, value: str):
+        self._hash_map[key] = value
+
+    def delete(self, key: str):
+        self._hash_map.pop(key)
+
+    def delete_all(self):
+        self._hash_map = dict()
