9f08e2c2d0e2c069788bd12fc93e710e291fbb33
JObog <jobogbaimhe@gmail.com>
2022-10-16 10:00:18 -0400

    removing old lkv package

--- lkv/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from lkv.utils import parse_config
-
-cfg = parse_config() 
-HOST = cfg['Connection']['host']
-PORT = int(cfg['Connection']['port'])
-KV_DIR = os.path.join(os.getcwd(), cfg['FileSys']['kv_dir'])
-KV_SOURCE = os.path.join(KV_DIR, cfg['FileSys']['kv_source'])
--- lkv/utils.py
+++
@@ -1,7 +0,0 @@
-import toml
-
-CONFIG_FILE = 'lkv.config'
-
-def parse_config():
-    with open(CONFIG_FILE, 'r') as file:
-        return toml.loads(file.read())
