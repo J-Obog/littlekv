69d01272b12c8ddf06a3858cb3425609de1913f4
JObog <jobogbaimhe@gmail.com>
2022-10-16 16:58:11 -0400

    adding read write lock decorators

--- littlekv/server/manager.py
+++ littlekv/server/manager.py
@@ -1,5 +1,5 @@
 from littlekv.store import Store
-from typing import Dict
+from typing import Callable, Dict
 from threading import Lock
 
 class LittleKVManager:
@@ -8,21 +8,35 @@ class LittleKVManager:
         self._hash_map = store.read()
         self._map_lock = Lock()
 
+    def r_locked(self, func: Callable):
+        def wraps(*args, **kwargs):
+            with self._map_lock:
+                return func(*args, **kwargs)
+        return wraps
+
+    def w_locked(self, func: Callable):
+        def wraps(*args, **kwargs):
+            with self._map_lock:
+                func(*args, **kwargs)
+                self._store.write(dict(self._hash_map))
+        return wraps
+
+    @r_locked
     def get(self, key: str) -> str:
-        with self._map_lock:
-            return self._hash_map.get(key, None)
+        return self._hash_map.get(key, None)
 
+    @r_locked
     def get_all(self) -> Dict[str,str]:
-        with self._map_lock:
-            return dict(self._hash_map)
+        return dict(self._hash_map)
 
+    @w_locked
     def set(self, key: str, value: str):
-        with self._map_lock:
-            self._hash_map[key] = value
+        self._hash_map[key] = value
 
+    @w_locked
     def delete(self, key: str):
-        with self._map_lock:
-            self._hash_map.pop(key)
+        self._hash_map.pop(key)
 
+    @w_locked
     def delete_all(self):
         self._hash_map = dict()
