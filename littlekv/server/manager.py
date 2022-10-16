ab7e4852139c716935801fc370d62445038ba509
JObog <jobogbaimhe@gmail.com>
2022-10-16 17:58:38 -0400

    getting rid of decorators

--- littlekv/server/manager.py
+++ littlekv/server/manager.py
@@ -11,39 +11,29 @@ class LittleKVManager:
         self._store_lock = Lock()
         self._pool = ThreadPoolExecutor()
 
-    def _r_locked(self, func: Callable):
-        def wraps(*args, **kwargs):
-            with self._map_lock:
-                return func(*args, **kwargs)
-        return wraps
-
     def _write(self, snapshot: Dict[str,str]):
         with self._store_lock:
             self._store.write(snapshot)
 
-    def _w_locked(self, func: Callable):
-        def wraps(*args, **kwargs):
-            with self._map_lock:
-                func(*args, **kwargs)
-                self._pool.submit(self._write, dict(self._hash_map))
-        return wraps
-
-    @_r_locked
     def get(self, key: str) -> str:
-        return self._hash_map.get(key, None)
+        with self._map_lock:
+            return self._hash_map.get(key, None)
 
-    @_r_locked
     def get_all(self) -> Dict[str,str]:
-        return dict(self._hash_map)
+        with self._map_lock:
+            return dict(self._hash_map)
 
-    @_w_locked
     def set(self, key: str, value: str):
-        self._hash_map[key] = value
+        with self._map_lock:
+            self._hash_map[key] = value
+            self._pool.submit(self._write, dict(self._hash_map))
 
-    @_w_locked
     def delete(self, key: str):
-        self._hash_map.pop(key)
+        with self._map_lock:
+            self._hash_map.pop(key)
+            self._pool.submit(self._write, dict(self._hash_map))
 
-    @_w_locked
     def delete_all(self):
-        self._hash_map = dict()
+        with self._map_lock:
+            self._hash_map = dict()
+            self._pool.submit(self._write, dict(self._hash_map))
---
+++ tests/unit/test_manager.py
@@ -0,0 +1,6 @@
+from littlekv.server import LittleKVManager
+from unittest.mock import Mock
+
+class TestManager:
+    def test_example(self):
+        assert 1 + 1 == 2
