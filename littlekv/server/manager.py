4e245a70af5afb2399ad862c5d9c12535ab1122f
JObog <jobogbaimhe@gmail.com>
2022-10-16 15:53:40 -0400

    adding lock for map in manager

--- littlekv/server/manager.py
+++ littlekv/server/manager.py
@@ -1,22 +1,28 @@
 from littlekv.store import Store
 from typing import Dict
+from threading import Lock
 
 class LittleKVManager:
     def __init__(self, store: Store):
         self._store = store
         self._hash_map = store.read()
+        self._map_lock = Lock()
 
     def get(self, key: str) -> str:
-        return self._hash_map.get(key, None)
+        with self._map_lock:
+            return self._hash_map.get(key, None)
 
     def get_all(self) -> Dict[str,str]:
-        return dict(self._hash_map)
+        with self._map_lock:
+            return dict(self._hash_map)
 
     def set(self, key: str, value: str):
-        self._hash_map[key] = value
+        with self._map_lock:
+            self._hash_map[key] = value
 
     def delete(self, key: str):
-        self._hash_map.pop(key)
+        with self._map_lock:
+            self._hash_map.pop(key)
 
     def delete_all(self):
         self._hash_map = dict()
