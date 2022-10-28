da9034e673a66546d8144422ffd31c21e1359c11
JObog <jobogbaimhe@gmail.com>
2022-10-27 20:03:25 -0400

    building hash map from list of commands

--- littlekv/server/manager.py
+++ littlekv/server/manager.py
@@ -1,19 +1,42 @@
-from tracemalloc import Snapshot
-from littlekv import store
-from littlekv.store import Store
-from typing import Callable, Dict
+from ast import arg
+from typing import List, Dict
 from threading import Lock
 from concurrent.futures import ThreadPoolExecutor
-from contextlib import contextmanager
+from littlekv.store.entry import MutationEntry, Operation
+from littlekv.store.store import Store
+
+def map_from_entries(entries: List[MutationEntry]) -> Dict[str,str]:
+    hash_map = dict()
+    sorted_entries = sorted(entries, key=lambda entry: entry.timestamp)
+    
+    for entry in sorted_entries:
+        op = entry.operation
+        args = entry.params 
+
+        if op == Operation.SET:
+            key = args[0]
+            val = args[1]
+            hash_map[key] = val
+        elif op == Operation.DELETE:
+            key = args[0] 
+            hash_map.pop(key)
+        elif op == Operation.FLUSH:
+            hash_map.clear()
+    
+    return hash_map
+
 
 class LittleKVManager:
     def __init__(self, store: Store, write_async=True):
         self._store = store
         self._write_async = write_async
-        self._hash_map = store.read()
+        self._hash_map = map_from_entries(store.read())
         self._map_lock = Lock()
         self._store_lock = Lock()
         self._pool = ThreadPoolExecutor()
+
+    def _init_map(self):
+        pass       
 
     def _write(self, data: Dict[str,str]):
         with self._store_lock:
